bdb922858ba779e83b2d0350857d1c305b36df54
Mariyan Sokolov <marian.sokolov@diadraw.com>
2023-05-03 15:48:11 +0300

    dsa

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { SlowService } from './app.slow.service';
+import { SlowService } from './slow/services/slow.service';
 
 @Controller()
 export class AppController {
@@ -8,6 +8,7 @@ export class AppController {
   @Get('/slow-job')
   async addSlowJob(): Promise<any> {
     console.log('11111');
+    const result = await this.slowService.addSlowJob({ someData: 'data' });
     return result;
   }
 }
